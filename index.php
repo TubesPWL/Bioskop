3eda1ded927cd0ca98864c1bd7baa681986928ca
danielusmanij <36160174+danielusmanij@users.noreply.github.com>
2019-12-30 07:49:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><title>Big Cinema</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1250">
+    <!-- ImageReady Preload Script (index.psd) -->
+    <script type="text/javascript">
+        <!--
+        function newImage(arg) {
+            if (document.images) {
+                rslt = new Image();
+                rslt.src = arg;
+                return rslt;
+            }
+        }
+        function changeImages() {
+            if (document.images && (preloadFlag == true)) {
+                for (var i=0; i<changeImages.arguments.length; i+=2) {
+                    document[changeImages.arguments[i]].src = changeImages.arguments[i+1];
+                }
+            }
+        }
+        var preloadFlag = false;
+        function preloadImages() {
+            if (document.images) {
+                home_over = newImage("images/home-over.jpg");
+                about_over = newImage("images/about-over.jpg");
+                movies_over = newImage("images/movies-over.jpg");
+                services_over = newImage("images/services-over.jpg");
+                contacts_over = newImage("images/contacts-over.jpg");
+                preloadFlag = true;
+            }
+        }
+        // -->
+    </script><!-- End Preload Script -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body leftmargin="0" topmargin="0" onload="preloadImages();" style="background-color: rgb(255, 255, 255);" marginheight="0" marginwidth="0">
+<center>
+    <!-- ImageReady Slices (index.psd) -->
+    <table class="frame" id="Table_01" border="0" cellpadding="0" cellspacing="0" height="681" width="776">
+        <tbody>
+        <tr>
+            <td rowspan="11"> <img src="images/main.jpg" alt="" height="680" width="100"></td>
+            <td colspan="16"> <img src="images/main-03.jpg" alt="" height="105" width="575"></td>
+            <td rowspan="11"> <img src="images/main-04.jpg" alt="" height="680" width="100"></td>
+            <td> <img src="images/spacer.gif" alt="" height="105" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="4"> <img src="images/main-05.jpg" alt="" height="45" width="193"></td>
+            <td colspan="3"> <a href="#" onmouseover="window.status='home'; changeImages('home', 'images/home-over.jpg'); return true;" onmouseout="window.status=''; changeImages('home', 'images/home.jpg'); return true;" onmousedown="changeImages('home', 'images/home-over.jpg'); return true;" onmouseup="changeImages('home', 'images/home-over.jpg'); return true;">
+                    <img name="home" src="images/home.jpg" alt="home" border="0" height="45" width="63"></a></td>
+            <td colspan="3"> <a href="#" onmouseover="window.status='about'; changeImages('about', 'images/about-over.jpg'); return true;" onmouseout="window.status=''; changeImages('about', 'images/about.jpg'); return true;" onmousedown="changeImages('about', 'images/about-over.jpg'); return true;" onmouseup="changeImages('about', 'images/about-over.jpg'); return true;">
+                    <img name="about" src="images/about.jpg" alt="about" border="0" height="45" width="67"></a></td>
+            <td colspan="2"> <a href="#" onmouseover="window.status='movies'; changeImages('movies', 'images/movies-over.jpg'); return true;" onmouseout="window.status=''; changeImages('movies', 'images/movies.jpg'); return true;" onmousedown="changeImages('movies', 'images/movies-over.jpg'); return true;" onmouseup="changeImages('movies', 'images/movies-over.jpg'); return true;">
+                    <img name="movies" src="images/movies.jpg" alt="movies" border="0" height="45" width="69"></a></td>
+            <td> <a href="#" onmouseover="window.status='services'; changeImages('services', 'images/services-over.jpg'); return true;" onmouseout="window.status=''; changeImages('services', 'images/services.jpg'); return true;" onmousedown="changeImages('services', 'images/services-over.jpg'); return true;" onmouseup="changeImages('services', 'images/services-over.jpg'); return true;">
+                    <img name="services" src="images/services.jpg" alt="services" border="0" height="45" width="77"></a></td>
+            <td colspan="3"> <a href="#" onmouseover="window.status='contacts'; changeImages('contacts', 'images/contacts-over.jpg'); return true;" onmouseout="window.status=''; changeImages('contacts', 'images/contacts.jpg'); return true;" onmousedown="changeImages('contacts', 'images/contacts-over.jpg'); return true;" onmouseup="changeImages('contacts', 'images/contacts-over.jpg'); return true;">
+                    <img name="contacts" src="images/contacts.jpg" alt="contacts" border="0" height="45" width="106"></a></td>
+            <td> <img src="images/spacer.gif" alt="" height="45" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="5" rowspan="2"> <img src="images/main-16.jpg" alt="" height="134" width="207"></td>
+            <td colspan="11"> <img src="images/wel.jpg" alt="" height="47" width="368"></td>
+            <td> <img src="images/spacer.gif" alt="" height="47" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="10" align="left" background="images/b_wel.jpg" height="87" valign="top" width="329"><span class="text1">
+                </span><br> <a href="#" class="a">details</a></td>
+            <td rowspan="7"> <img src="images/main-19.jpg" alt="" height="397" width="39"></td>
+            <td> <img src="images/spacer.gif" alt="" height="87" width="1"></td>
+        </tr>
+        <tr>
+            <td rowspan="6"> <img src="images/main-20.jpg" alt="" height="310" width="39"></td>
+            <td colspan="7"> <img src="images/services-21.jpg" alt="" height="56" width="228"></td>
+            <td rowspan="6"> <img src="images/main-22.jpg" alt="" height="310" width="41"></td>
+            <td colspan="6" rowspan="3"> <img src="images/movies-23.jpg" alt="" height="187" width="228"></td>
+            <td> <img src="images/spacer.gif" alt="" height="56" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="2"> <img src="images/services-24.jpg" alt="" height="106" width="74"></td>
+            <td colspan="3" class="text1" align="left" bgcolor="#c9a873" height="106" valign="top" width="139">Ea consetetur sadi lorem ipsum dolor sed amet
+                consetetur sadi lorem ipsum dolor sed amet Ea consetetur sadi lorem
+                ipsum dolor sed</td>
+            <td colspan="2" rowspan="5"> <img src="images/services-25.jpg" alt="" height="254" width="15"></td>
+            <td> <img src="images/spacer.gif" alt="" height="106" width="1"></td>
+        </tr>
+        <tr>
+            <td rowspan="4"> <img src="images/services-26.jpg" alt="" height="148" width="32"></td>
+            <td colspan="4" rowspan="3" align="left" bgcolor="#c9a873" height="143" valign="top" width="181"><br>
+                <br>
+                <a href="#" class="a">details</a></td>
+            <td> <img src="images/spacer.gif" alt="" height="25" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="2" rowspan="3"> <img src="images/movies-27.jpg" alt="" height="123" width="37"></td>
+            <td colspan="3" align="left" bgcolor="#c9a873" height="110" valign="top" width="159">
+                <p><span class="text1"></span>
+                    <a href="#" class="a">details</a></p>
+            </td>
+            <td rowspan="3"> <img src="images/movies-28.jpg" alt="" height="123" width="32"></td>
+            <td> <img src="images/spacer.gif" alt="" height="110" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="3" rowspan="2"> <img src="images/movies-29.jpg" alt="" height="13" width="159"></td>
+            <td> <img src="images/spacer.gif" alt="" height="8" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="4"> <img src="images/services-30.jpg" alt="" height="5" width="181"></td>
+            <td> <img src="images/spacer.gif" alt="" height="5" width="1"></td>
+        </tr>
+        <tr>
+            <td colspan="16" align="center" background="images/b_links.jpg" height="86" valign="middle" width="575"><a href="#">Home</a>
+                | <a href="#">About us</a> | <a href="#">Movies</a>
+                | <a href="#">Services</a> | <a href="#">Tickets</a>
+                | <a href="#">Login</a> | <a href="#">login</a>
+                Copyright © Your Company Name. Designed by <a href="http://www.templatesland.com" target="_blank">TemplatesLand</a><br>
+                Supported by <a href="http://www.hosting24.com/" target="_blank">Hosting24.com</a> </td>
+            <td> <img src="images/spacer.gif" alt="" height="86" width="1"></td>
+        </tr>
+        <tr>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="100"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="39"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="32"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="42"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="80"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="14"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="45"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="4"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="11"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="41"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="15"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="22"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="47"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="77"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="35"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="32"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="39"></td>
+            <td> <img src="images/spacer.gif" alt="" height="1" width="100"></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+    <!-- End ImageReady Slices -->
+</center>
+</body></html>
